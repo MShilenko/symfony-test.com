091e9495c6424b42fb4395400132b5e91d90ef0d
dms-mshilenko <shilenkomp@mail.ru>
2019-12-16 14:11:48 +0300

    add form to Post

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Cocur\Slugify\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
@@ -28,6 +31,30 @@ class PostsController extends AbstractController
         ]);
     }
 
+     /**
+     * @Route("/posts/new", name="new_blog_post")
+     */
+    public function addPost(Request $request, Slugify $slugify)
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($slugify->slugify($post->getTitle()));
+            $post->setCreatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_posts');
+        }
+        return $this->render('posts/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
     /**
      * @Route("/posts/{slug}", name="blog_show")
      */
@@ -37,4 +64,5 @@ class PostsController extends AbstractController
             'post' => $post
         ]);
     }
+
 }
