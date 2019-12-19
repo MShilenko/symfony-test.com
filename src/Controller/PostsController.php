55d37f571945bd9cd4626d37dd3282e2bf4cc3e8
dms-mshilenko <shilenkomp@mail.ru>
2019-12-19 14:41:00 +0300

    registration start

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -56,6 +56,19 @@ class PostsController extends AbstractController
     }
 
     /**
+     * @Route("/posts/search", name="blog_search")
+     */
+    public function search(Request $request)
+    {
+        $query = $request->query->get('q');
+        $posts = $this->postRepository->searchByQuery($query);
+
+        return $this->render('posts/query_post.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
      * @Route("/posts/{slug}", name="blog_show")
      */
     public function post(Post $post)
